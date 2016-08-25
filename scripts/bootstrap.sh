96f5e177012ef5ead0f1da0c25231799993f0067
Christophe Delcourt <c.delcourt@base2services.com>
2016-08-25 10:41:59 +1000

    Minor fixes for docker, now not using screen

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -39,6 +39,6 @@ chmod -R 777 /data
 chown 1000:1000 /data/jenkins /data/jenkins-dind
 
 # Configure and run the Ciinabox containers in screen
-screen -S nginx-proxy -d -m docker run --name nginx-proxy -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
-screen -S ciinabox-jenkins -d -m docker run --name ciinabox-jenkins -e VIRTUAL_HOST=$DOMAIN_NAME -e VIRTUAL_PORT=8080 -v /data/jenkins:/var/jenkins_home base2/ciinabox-jenkins:2
-screen -S ciinabox-slave-jenkins -d -m docker run --name ciinabox-slave-jenkins --privileged=true -e PORT=4444 -p 4444:4444 -p 2223:22 -v /data/jenkins-dind/:/var/lib/docker base2/ciinabox-jenkins-slave start-dind
+docker run -d --name nginx-proxy -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
+docker run -d --name ciinabox-jenkins -e VIRTUAL_HOST=$DOMAIN_NAME -e VIRTUAL_PORT=8080 -v /data/jenkins:/var/jenkins_home base2/ciinabox-jenkins:2
+docker run -d --name ciinabox-slave-jenkins --privileged=true -e PORT=4444 -p 4444:4444 -p 2223:22 -v /data/jenkins-dind/:/var/lib/docker base2/ciinabox-jenkins-slave start-dind
